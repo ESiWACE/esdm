7b9aba01f98c528989a4b3c764928014588e00f1
Julian M. Kunkel <juliankunkel@googlemail.com>
2016-04-28 16:19:52 +0200

    Updated build; support for doxygen and plantuml (documentation).

--- src/memvol.c
+++ src/memvol.c
@@ -13,6 +13,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with h5-memvol.  If not, see <http://www.gnu.org/licenses/>.
 
+
 #include <stdlib.h>
 #include <assert.h>
 
