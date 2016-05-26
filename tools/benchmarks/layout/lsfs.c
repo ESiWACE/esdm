96682756406bfded34ea4fed518a3d39c92e8e65
Julian M. Kunkel <juliankunkel@googlemail.com>
2016-05-26 17:04:38 +0200

    Test program. Todo: fix computation.

--- tools/benchmarks/layout/lsfs.c
+++ tools/benchmarks/layout/lsfs.c
@@ -32,6 +32,7 @@
 #include <unistd.h>
 #include <assert.h>
 #include <string.h>
+#include <stdint.h>
 #include <mpi.h>
 
 #include "mtime.h"
@@ -161,26 +162,39 @@ void lfsWrite(){
 typedef struct{
   size_t pos;
   size_t size;
-} lfs_record;
-
+} lfs_record_on_disk;
 
 typedef struct{
+  size_t pos;
   size_t size;
+  size_t file_position;
+} lfs_record;
+
+typedef struct{
+  int size;
   lfs_record * records;
+  lfs_record * records_end; // sorted by the end position of an record
 } lfs_index;
 
 
 
-int lfsRecordComparePos(const void * u1, const void * v1){
-  const lfs_record * u = (const lfs_record *) u1;
-  const lfs_record * v = (const lfs_record *) v1;
+static int lfsRecordComparePos(const void * u1, const void * v1){
+  const lfs_record_on_disk * u = (const lfs_record_on_disk *) u1;
+  const lfs_record_on_disk * v = (const lfs_record_on_disk *) v1;
 
   return u->pos - v->pos;
 }
 
-void lfsPrintIndex(lfs_index * index){
-  for(int i=0; i < index->size; i++){
-    printf("%zu %zu\n", index->records[i].pos, index->records[i].size);
+static int lfsRecordCompareEndPos(const void * u1, const void * v1){
+  const lfs_record_on_disk * u = (const lfs_record_on_disk *) u1;
+  const lfs_record_on_disk * v = (const lfs_record_on_disk *) v1;
+
+  return u->pos + u->size - (v->pos + v->size);
+}
+
+void lfsPrintIndex(lfs_record * r, int size){
+  for(int i=0; i < size; i++){
+    printf("%d pos: %zu size: %zu file: %zu\n", i, r[i].pos, r[i].size, r[i].file_position);
   }
 }
 
@@ -194,32 +208,170 @@ lfs_index * lfsReadIndex(){
   ret = stat(lfsfilename, & stats);
   assert(ret == 0);
 
-  int record_count = stats.st_size / sizeof(lfs_record);
+  int record_count = stats.st_size / sizeof(lfs_record_on_disk);
 
   lfs_record * records = malloc(sizeof(lfs_record) * record_count);
 
+  size_t file_position = 0;
   FILE * lfs = fopen(lfsfilename, "r");
   for(int i=0; i < record_count; i++){
-    ret = fread(& records[i], sizeof(lfs_record), 1, lfs);
+    ret = fread(& records[i], sizeof(lfs_record_on_disk), 1, lfs);
+    records[i].file_position = file_position;
     assert( ret == 1 );
     data ++;
+    file_position += records[i].size;
   }
   fclose(lfs);
 
   // do the sort
   qsort(records, record_count, sizeof(lfs_record), lfsRecordComparePos);
 
+  lfs_record * records_end = malloc(sizeof(lfs_record) * record_count);
+  memcpy(records_end, records, sizeof(lfs_record) * record_count);
+  qsort(records_end, record_count, sizeof(lfs_record), lfsRecordCompareEndPos);
+
   t = stop_timer(t1);
 
-  data *= sizeof(lfs_record);
+  data *= sizeof(lfs_record_on_disk);
 
   printf("lfsReadIndex: %f %zu bytes = %.1f MiB/s\n", t, data, data / t / 1024.0 / 1024 );
   lfs_index * index = (lfs_index*) malloc(sizeof(lfs_index));
-  *index = (lfs_index){record_count, records};
+  *index = (lfs_index){record_count, records, records_end};
   return index;
 }
 
+static inline int lfs_binsearch_left(off_t start, lfs_record * r, int size){
+  int l = 0;
+  int u = size - 1;
+  int p = (l + u) / 2;
 
+  while(l < u){
+    //printf("bsl %d %d (%d): start %zu r.pos %zu r.sum %zu\n", l, u, p, start,  r[p].pos, r[p].size +r[p].pos);
+    int64_t d = ((int64_t) start) - (r[p].pos + r[p].size);
+
+    if ( d < 0 ){
+      // left side
+      u = p - 1;
+    }else if(d > 0){
+      l = p + 1;
+    }else{ // equal
+      return p;
+    }
+    p = (l + u) / 2;
+  }
+
+  //printf("xl %zu pos=%zu size=%zu\n", start, r[p].pos, r[p].size);
+  return p;
+}
+
+static inline int lfs_binsearch_right(off_t end, lfs_record * r, int size){
+  int l = 0;
+  int u = size - 1;
+  int p = (l + u) / 2;
+
+  while(l < u){
+    //printf("%d %d (%d): %zu %zu\n", l, u, p, offset,  r[p].pos);
+    int64_t d = ((int64_t) end) - r[p].pos;
+
+    if ( d < 0 ){
+      // left side
+      u = p - 1;
+    }else if(d > 0){
+      l = p + 1;
+    }else{ // equal
+      return p;
+    }
+    p = (l + u) / 2;
+  }
+  //printf("xr %lld\n", ((int64_t) offset) - r[p].pos);
+  //if(r[p].pos < offset){
+    //printf("HERE %d %lld < %lld\n", p, offset, r[p].pos);
+  //}
+  return p;
+}
+
+
+size_t lfs_pread(int fd, void *buf, size_t size, size_t offset, lfs_index * index){
+  printf("pread %zu - %zu\n", offset, size);
+
+  // we have to find all overlapping regions in the index.
+  // there are two options for overlaps
+  // left side overlap, i.e., r->pos <= offset && r->pos + r->size > offset
+  // right side overlap, i.e., r->pos > offset && offset + size > r->pos
+  int cur_pos = lfs_binsearch_left(offset + size, index->records_end, index->size);
+  printf(" l %d\n", cur_pos);
+
+  lfs_record * r = & index->records[cur_pos];
+
+  size_t end = offset + size;
+  // left side overlaps
+  while(cur_pos >= 0 && (r->pos + r->size) > offset){
+    printf("l %lu %lu\n", r->pos, r->size);
+    cur_pos--;
+    r = & index->records[cur_pos];
+  }
+
+  cur_pos = lfs_binsearch_right(offset, index->records, index->size);
+  r = & index->records[cur_pos];
+  //printf(" r %d %lu %lu\n", start_pos, r->pos, r->size);
+  // right side overlaps
+  while( cur_pos < index->size && r->pos < end){
+    printf("r %lu %lu\n", r->pos, r->size);
+    cur_pos++;
+    r = & index->records[cur_pos];
+  }
+
+  return size;
+}
+
+void lfsReadAll(lfs_index * index){
+  // we read one variable from each block
+  int fd = open(filename, O_RDONLY);
+  size_t ret = 0;
+
+  timer t1;
+  double t = 0;
+  start_timer(& t1);
+
+  // determine start position
+  size_t startPos = 0;
+  for(int i=0; i < timesteps; i++){
+    ret = lfs_pread(fd, buffer, blocksize, startPos, index);
+    assert(ret == blocksize);
+
+    startPos += blocksize;
+  }
+  close(fd);
+  size_t data = timesteps * blocksize;
+
+  t = stop_timer(t1);
+  printf("lfsReadAll: %f %zu bytes = %.1f MiB/s\n", t, data, data / t / 1024.0 / 1024 );
+}
+
+void lfsRead1DVar(lfs_index * index){
+  // we read one variable from each block
+  int fd = open(filename, O_RDONLY);
+  size_t ret = 0;
+
+  timer t1;
+  double t = 0;
+  start_timer(& t1);
+
+  // determine start position
+  size_t startPos = rand() % (blocksize - smalldatasize);
+
+  for(int i=0; i < timesteps; i++){
+    ret = lfs_pread(fd, buffer, smalldatasize, startPos, index);
+    assert(ret == smalldatasize);
+
+    startPos += blocksize;
+  }
+  close(fd);
+  size_t data = timesteps * smalldatasize;
+
+  t = stop_timer(t1);
+  printf("stdRead1DVar: %f %zu bytes = %.1f MiB/s\n", t, data, data / t / 1024.0 / 1024 );
+}
 
 void cleanCache(){
   int ret;
@@ -255,7 +407,13 @@ int main(int argc, char ** argv){
   lfsWrite();
   cleanCache();
   lfs_index * index = lfsReadIndex();
-  //lfsPrintIndex(index);
+  //lfsPrintIndex(index->records, index->size);
+  //printf("End:\n");
+  //lfsPrintIndex(index->records_end, index->size);
+
+  lfsReadAll(index);
+  cleanCache();
+  lfsRead1DVar(index);
 
   return 0;
 }
