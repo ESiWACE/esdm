891d954d2cdbe8857032ea6f03d551228baa7af9
Olga Kravtsova <okravt@yandex.ru>
2016-08-21 22:35:10 +0200

    Dataset create (nicht fehlerfrei), commit wegen merge

--- src/m-dataset.c
+++ src/m-dataset.c
@@ -12,3 +12,52 @@
 // 
 // You should have received a copy of the GNU Lesser General Public License
 // along with h5-memvol.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <stdio.h>
+#include <string.h>
+
+
+static void* memvol_dataset_create(void* object, H5VL_loc_params_t loc_params, const char* name, hid_t dcpl_id, hid_t dapl_id, hid_t dxpl_id, void** req)
+   
+{
+    int *datatype_value, *dataspace_value;
+
+
+    puts("------------ memvol_dataset_create() called ------------");    
+   
+   memvol_dataset_t*  dataset = (memvol_dataset_t*)malloc(sizeof(memvol_dataset_t));
+
+
+// name of dataset
+     dataset->name = (char*)malloc(strlen(name));
+
+// retrieving of datatype
+
+ 
+    H5Pget(dcpl_id, H5VL_PROP_DSET_SPACE_ID, datatype_value);
+    dataset->datatype = (void*) malloc(sizeof(datatype_value));
+
+// retrieving of dataspace
+    H5Pget(dcpl_id, H5VL_PROP_DSET_SPACE_ID, dataspace_value);
+    dataset->dataspace = (void*) malloc(sizeof(dataspace_value));
+
+// retrieving of link creation property list´s
+//   H5Pget(dcpl_id, H5VL_DSET_LCPL_ID, *lcpl);
+//   dataset->dataspace = malloc(sizeof(*lcpl));
+
+
+}
+
+static herr_t memvol_dataset_close(void* dset, hid_t dxpl_id, void** req) {
+
+    printf("------------ memvol_group_close() called --------------");
+
+    memvol_dataset_t *dataset = (memvol_dataset_t*) dset;
+
+    free(dataset->name);
+    free(dataset->datatype);
+    free(dataset->dataspace);
+    free(dataset);
+
+    return 1;
+}
