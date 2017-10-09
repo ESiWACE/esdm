07439b21709a246fe23d4613936b24ea746c9cf2
Jakob Luettgau <luettgau@dkrz.de>
2017-10-09 12:32:51 +0200

    license notice

--- src/test/readwrite.c
+++ src/test/readwrite.c
@@ -1,3 +1,19 @@
+/* This file is part of ESDM.                                              
+ *                                                                              
+ * This program is is free software: you can redistribute it and/or modify         
+ * it under the terms of the GNU Lesser General Public License as published by  
+ * the Free Software Foundation, either version 3 of the License, or            
+ * (at your option) any later version.                                          
+ *                                                                              
+ * This program is is distributed in the hope that it will be useful,           
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of               
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                
+ * GNU General Public License for more details.                                 
+ *                                                                                 
+ * You should have received a copy of the GNU Lesser General Public License        
+ * along with ESDM.  If not, see <http://www.gnu.org/licenses/>.           
+ */                                                                         
+
 /*
  * This test uses the ESDM high-level API to actually write a contiuous ND subset of a data set
  */
