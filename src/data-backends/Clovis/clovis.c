2a93c6c233bb31c310223508c745f08b4267fdef
Hua Huang <hua.huang@seagate.com>
2017-10-18 18:07:29 +0800

    Initial thought for Clovis backend design.

--- src/data-backends/Clovis/clovis.c
+++ src/data-backends/Clovis/clovis.c
@@ -12,6 +12,9 @@
  *                                                                                 
  * You should have received a copy of the GNU Lesser General Public License        
  * along with ESDM.  If not, see <http://www.gnu.org/licenses/>.           
+ *
+ * Original author: Huang Hua <hua.huang@seagate.com>
+ * Original creation date: 13-Oct-2017
  */                                                                         
 
 /**
@@ -20,3 +23,229 @@
  */
 
 #include<esdm.h>
+
+#if 0
+
+int esdm_backend_mero_init(char * conf, struct esdm_backend_generic **eb_out)
+{
+	struct esdm_backend_mero *ebm;
+	int rc;
+
+	ebm = malloc(sizeof *ebm);
+	if (ebm == NULL)
+		return -ENOMEM;
+
+	rc = conf_parse(conf, ebm);
+	if (rc != 0) {
+		free(ebm);
+		return rc;
+	}
+
+        /* Clovis instance */
+        rc = m0_clovis_init(&ebm->ebm_clovis_instance, &ebm->ebm_clovis_conf, true);
+        if (rc != 0) {
+                printf("Failed to initilise Clovis\n");
+		free(ebm);
+		return rc;
+        }
+
+        /* And finally, clovis root realm */
+        m0_clovis_container_init(&ebm->ebm_clovis_container,
+                                 NULL, &M0_CLOVIS_UBER_REALM,
+                                 ebm->ebm_clovis_instance);
+        rc = ebm->ebm_clovis_container.co_realm.re_entity.en_sm.sm_rc;
+
+        if (rc != 0) {
+                printf("Failed to open uber realm\n");
+		free(ebm);
+		return rc;
+        }
+
+	out = &ebm->ebm_base;
+	return 0;
+}
+
+int esdm_backend_mero_fini(struct esdm_backend_generic *eb)
+{
+	struct esdm_backend_mero *ebm;
+
+	ebm = container_of(eb, struct esdm_backend_mero, ebm_base);
+	m0_clovis_fini(ebm->ebm_clovis_instance, true);
+	free(ebm);
+	return 0;
+}
+static void open_entity(struct m0_clovis_obj *obj)
+{
+	struct m0_clovis_entity *entity = &obj->ob_entity;
+	struct m0_clovis_op *ops[1] = { NULL };
+
+	m0_clovis_entity_open(entity, &ops[0]);
+	m0_clovis_op_launch(ops, 1);
+	m0_clovis_op_wait(ops[0], M0_BITS(M0_CLOVIS_OS_FAILED,
+					  M0_CLOVIS_OS_STABLE),
+			  m0_time_from_now(3,0));
+	m0_clovis_op_fini(ops[0]);
+	m0_clovis_op_free(ops[0]);
+	ops[0] = NULL;
+}
+
+static int create_object(struct esdm_backend_mero *ebm,
+			 struct m0_uint128 id)
+{
+	int                  rc = 0;
+	struct m0_clovis_obj obj;
+	struct m0_clovis_op *ops[1] = {NULL};
+
+	memset(&obj, 0, sizeof(struct m0_clovis_obj));
+
+	m0_clovis_obj_init(&obj, &ebm->ebm_clovis_container.clovis_uber_realm,
+			   &id,
+			   m0_clovis_default_layout_id(ebm->ebm_clovis_instance));
+
+	open_entity(&obj);
+
+	m0_clovis_entity_create(&obj.ob_entity, &ops[0]);
+
+	m0_clovis_op_launch(ops, ARRAY_SIZE(ops));
+
+	rc = m0_clovis_op_wait(
+		ops[0], M0_BITS(M0_CLOVIS_OS_FAILED, M0_CLOVIS_OS_STABLE),
+		m0_time_from_now(3,0));
+
+	m0_clovis_op_fini(ops[0]);
+	m0_clovis_op_free(ops[0]);
+	//m0_clovis_entity_fini(&obj.ob_entity);
+	m0_clovis_obj_fini(&obj);
+
+	return rc;
+}
+
+
+int esdm_backend_mero_alloc(struct esdm_backend_generic *eb,
+			    int       n_dims,
+			    int*      dims_size,
+			    esdm_type type,
+			    char*     md1,
+			    char*     md2,
+			    char**    out_object_id,
+			    char**    out_mero_metadata)
+{
+	struct esdm_backend_mero *ebm;
+	struct m0_uint128         obj_id;
+	int                       rc;
+
+	ebm = container_of(eb, struct esdm_backend_mero, ebm_base);
+
+	/* First step: alloc a new fid for this new object. */
+	obj_id = object_id_alloc();
+
+	/* Then create object */
+	rc = create_object(ebm, obj_id);
+	if (rc == 0) {
+		/* encode this obj_id into string */
+		*out_object_id = object_id_encode(obj_id);
+		*out_mero_metadata = object_meta_encode(obj_id);
+	}
+	return rc;
+}
+
+int esdm_backend_mero_open (struct esdm_backend_generic *eb,
+			    char*     object_id,
+			    void**    obj_handle)
+{
+	struct esdm_backend_mero *ebm;
+	struct m0_clovis_obj     *obj;
+	struct m0_uint128         obj_id;
+	int                       rc;
+
+	ebm = container_of(eb, struct esdm_backend_mero, ebm_base);
+
+	/* convert from json string to object id. */
+	obj_id = object_id_decode(object_id);
+
+	obj = maolloc(sizeof *obj);
+	if (obj == NULL)
+		return -ENOMEM;
+
+	memset(obj, 0, sizeof(struct m0_clovis_obj));
+	m0_clovis_obj_init(obj, &ebm->ebm_clovis_container.clovis_uber_realm,
+			   &obj_id,
+			   m0_clovis_default_layout_id(ebm->ebm_clovis_instance));
+
+	open_entity(obj);
+	*obj_handle = obj;
+
+	return 0;
+}
+
+#define PAGE_MASK (409ULL - 1)
+
+int esdm_backend_mero_write(struct esdm_backend_generic *eb,
+			    void*    obj_handle,
+			    uint64_t start,
+			    int      count,
+			    void*    data)
+{
+	struct m0_clovis_obj* ebm_obj = (struct m0_clovis_obj*)obj_handle;
+
+	/* First check if the start is block-aligned (4K-aligned). */
+	if (start & PAGE_MASK) {
+		/*
+		 * Read the first the page and modify the buffer.
+		 * A new buffer is allocated.
+		 */
+	}
+	if ((start + count) & PAGE_MASK) {
+		/*
+		 * Read the last page and modify the buffer.
+		 * A new buffer is allocated.
+		 */
+	}
+	/*
+	 * Write these buffers.
+	 */
+}
+
+int esdm_backend_mero_read (struct esdm_backend_generic *eb,
+			    void*    obj_handle,
+			    uint64_t start,
+			    int      count,
+			    void*    data)
+{
+	struct m0_clovis_obj* ebm_obj = (struct m0_clovis_obj*)obj_handle;
+
+	/* First check if the start is block-aligned (4K-aligned). */
+	if (start & PAGE_MASK) {
+		/*
+		 * extend the 'start'. A new buffer is allocated to
+		 * cover it.
+		 */
+	}
+	if ((start + count) & PAGE_MASK) {
+		/*
+		 * extend the 'count'. A new buffer is allocated to
+		 * cover it.
+		 */
+	}
+	/*
+	 * read the extended region and copy the data from new buffers.
+	 */
+}
+
+int esdm_backend_mero_close(struct esdm_backend_generic *eb,
+			    void*    obj_handle)
+{
+	struct esdm_backend_mero *ebm;
+	struct m0_clovis_obj     *obj;
+
+	ebm = container_of(eb, struct esdm_backend_mero, ebm_base);
+
+	obj = (struct m0_clovis_obj*)obj_handle;
+	m0_clovis_obj_fini(obj);
+	free(obj);
+
+	return 0;
+}
+
+
+#endif
--- src/data-backends/Clovis/clovis.h
+++ src/data-backends/Clovis/clovis.h
@@ -12,10 +12,108 @@
  *                                                                                 
  * You should have received a copy of the GNU Lesser General Public License        
  * along with ESDM.  If not, see <http://www.gnu.org/licenses/>.           
+ *
+ * Original author: Huang Hua <hua.huang@seagate.com>
+ * Original creation date: 13-Oct-2017
  */   
 #ifndef CLOVIS_H
 #define CLOVIS_H
 
 // Internal functions used by this backend.
+
+#if 0
+
+struct esdm_backend_generic {
+	uint64_t eb_magic;
+	char*    eb_name;
+	uint64_t eb_id;
+};
+
+struct esdm_backend_mero {
+	struct esdm_backend_generic ebm_base;
+
+	/* Mero Clovis */
+	struct m0_clovis*           ebm_clovis_instance;
+	struct m0_clovis_container  ebm_clovis_container;
+	struct m0_clovis_config     ebm_clovis_conf;
+
+	struct m0_fid               ebm_last_fid;
+
+};
+
+/**
+ * Allocate the backend context and initialise it.
+ * In this interface, Clovis connects to Mero cluster.
+ * @param [in] conf, the connection parameters.
+ * @param [out] eb_out, returned the backend context.
+ *
+ * The format of conf string would like this:
+ * "laddr ha_addr prof_opt proc_fid".
+ */
+int esdm_backend_mero_init(char * conf, struct esdm_backend_generic **eb_out);
+
+/**
+ * Finalise the backend.
+ */
+int esdm_backend_mero_fini(struct esdm_backend_generic *eb);
+
+
+/**
+ * Allocate an object or objects in backend and return them
+ * in @out out_object_id and their internal attributes @out out_mero_metadata.
+ * @param [in] eb, the backend pointer.
+ * @param [in] n_dims, number of dimensions.
+ * @param [in] dims_size, dimension array.
+ * @param [in] type, type of esdm.
+ * @param [in] md1, metadata.
+ * @param [in] md2, metadata.
+ * @param [out] out_object_id, the returned objects.
+ * @param [out] out_mero_metadata, the returned metadata.
+ */
+int esdm_backend_mero_alloc(struct esdm_backend_generic *eb,
+			    int       n_dims,
+			    int*      dims_size,
+			    esdm_type type,
+			    char*     md1,
+			    char*     md2,
+			    char**    out_object_id,
+			    char**    out_mero_metadata);
+/**
+ * Open an object for read/write.
+ *
+ * return object handle in [out] obj_handle;
+ */
+int esdm_backend_mero_open (struct esdm_backend_generic *eb,
+			    char*    object_id,
+			    void**   obj_handle);
+
+/**
+ * Write to an object.
+ */
+int esdm_backend_mero_write(struct esdm_backend_generic *eb,
+			    void*    obj_handle,
+			    uint64_t start,
+			    int      count,
+			    void*    data);
+
+/**
+ * Read from object.
+ */
+int esdm_backend_mero_read (struct esdm_backend_generic *eb,
+			    void*    obj_handle,
+			    uint64_t start,
+			    int      count,
+			    void*    data);
+
+/**
+ * Close an object.
+ */
+int esdm_backend_mero_close(struct esdm_backend_generic *eb,
+			    void*  obj_handle);
+
+
+
+#endif
+
 
 #endif
