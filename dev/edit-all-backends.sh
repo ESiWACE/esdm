a845188ed78da23a0e2b2b54a4604b872d50f9f1
Jakob Luettgau <luettgau@dkrz.de>
2018-03-15 16:05:46 +0100

    Include report data model description in documentation.

---
+++ dev/edit-all-backends.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Backend Plugins to edit / Files to open
+data=(POSIX/posix.c Clovis/clovis.c WOS/wos.c)
+meta=(metadummy/metadummy.c MongoDB/mongodb.c)
+
+
+# Hopefully no need to change the following ###################################
+cd ../src
+paths=()
+
+for i in ${data[@]}; do
+	echo $i
+	paths+=("backends-data/$i")
+done
+
+for i in ${meta[@]}; do
+	echo $i
+	paths+=("backends-metadata/$i")
+done
+
+
+$EDITOR -p ${paths[*]}
