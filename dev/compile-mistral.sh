0c907d93e6b5d6e6b540ca2acc7d858acac5945c
Julian M. Kunkel <juliankunkel@googlemail.com>
2018-05-11 18:17:35 +0100

    Minor fixes for running on Mistral.

---
+++ dev/compile-mistral.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+module load openmpi/1.8.4-gcc71 gcc/7.1.0 cmake betke/glib/2.48.0
+git clone https://github.com/akheron/jansson
+cd jansson
+autoreconf -fi
+./configure --prefix=$PWD/../deps
+make -j install
+
+export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$PWD/deps/lib/pkgconfig
+
+./configure --with-cc=/sw/rhel6-x64/gcc/gcc-7.1.0/bin/gcc
+cd build
+make -j
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/../deps/lib
