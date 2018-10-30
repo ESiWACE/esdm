8309ef74049ca941ca5a48b22df6db739d1aae04
Jakob Luettgau <luettgau@dkrz.de>
2018-10-30 09:10:06 +0100

    Fix getopt issues with configure script.

--- dev/devenv-install-netcdf-bench.sh
+++ dev/devenv-install-netcdf-bench.sh
@@ -14,11 +14,11 @@ mkdir -p install/download
 cd install/download
 
 
-# download most recent HDF5 source code
+# download source code
 if [[ ! -e netcdf-bench ]] ; then
 	echo "Downloading source code for $NAME"
-	#git clone https://github.com/joobog/netcdf-bench
-	cp -r /home/pq/ESiWACE/git/netcdf-bench .
+	git clone https://github.com/joobog/netcdf-bench
+	#cp -r /home/pq/ESiWACE/git/netcdf-bench .
 fi
 
 cd netcdf-bench
@@ -28,9 +28,7 @@ rm -rf build
 mkdir build
 cd build
 
-#echo cmake -DCMAKE_INSTALL_PREFIX:PATH=$prefix -DCMAKE_C_FLAGS=-L$prefix/lib/  ..
 cmake -DCMAKE_INSTALL_PREFIX:PATH=$prefix ..
-#cmake -DCMAKE_INSTALL_PREFIX:PATH=$prefix -DCMAKE_C_FLAGS=-L$prefix/lib/  ..
 
 
 make
