82adb2af6b5ab2d2dcaef0b971e906e8451ad161
Julian M. Kunkel <juliankunkel@googlemail.com>
2019-07-01 12:03:45 +0100

    ESDM MPI library header extracted.

---
+++ src/include/esdm-mpi.h
@@ -0,0 +1,12 @@
+#ifndef ESDM_MPI_H
+#define ESDM_MPI_H
+
+#include <mpi.h>
+
+int esdm_mpi_get_tasks_per_node();
+
+void esdm_mpi_distribute_config_file(char *config_filename);
+
+void esdm_mpi_init();
+
+#endif
--- src/test/readwrite-benchmark.c
+++ src/test/readwrite-benchmark.c
@@ -19,53 +19,15 @@
  */
 
 #include <assert.h>
-#include <esdm-internal.h>
-#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include <mpi.h>
+#include <esdm-internal.h>
+#include <esdm-mpi.h>
 #include "util/test_util.h"
 
-int esdm_mpi_get_tasks_per_node() {
-  MPI_Comm shared_comm;
-  int count = 1;
-
-  MPI_Comm_split_type(MPI_COMM_WORLD, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, &shared_comm);
-  MPI_Comm_size(shared_comm, &count);
-  MPI_Comm_free(&shared_comm);
-
-  return count;
-}
-
-void esdm_mpi_distribute_config_file(char *config_filename) {
-  int mpi_rank;
-  MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
-
-  char *config = NULL;
-  if (mpi_rank == 0) {
-    int len;
-    read_file(config_filename, &config);
-    len = strlen(config) + 1;
-    MPI_Bcast(&len, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    MPI_Bcast(config, len, MPI_CHAR, 0, MPI_COMM_WORLD);
-  } else {
-    int len;
-    MPI_Bcast(&len, 1, MPI_INT, 0, MPI_COMM_WORLD);
-    config = (char *)malloc(len);
-    MPI_Bcast(config, len, MPI_CHAR, 0, MPI_COMM_WORLD);
-  }
-  esdm_load_config_str(config);
-}
-
-void esdm_mpi_init() {
-  int mpi_size;
-  MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
-
-  int pPerNode = esdm_mpi_get_tasks_per_node();
-  esdm_set_procs_per_node(pPerNode);
-  esdm_set_total_procs(mpi_size);
-}
 
 int main(int argc, char *argv[]) {
   int provided;
