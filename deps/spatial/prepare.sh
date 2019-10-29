52b19f05ad364880d62b36f23d79eb69a63ed98c
Nathanael Hübbe <nathanael.huebbe@informatik.uni-hamburg.de>
2019-10-29 17:50:07 +0100

    fixed the libspatialindex scripts

--- deps/spatial/prepare.sh
+++ deps/spatial/prepare.sh
@@ -1,9 +1,11 @@
 #!/bin/bash -e
+sourceDir="$(readlink -f $(dirname $0))"
+
 if [[ ! -e libspatialindex ]] ; then
    git clone https://github.com/libspatialindex/libspatialindex.git
 fi
 pushd libspatialindex
-mkdir build
+mkdir -p build
 cd build
-cmake -DCMAKE_INSTALL_PREFIX:PATH=$PWD/../../install
+cmake -DCMAKE_INSTALL_PREFIX:PATH=$PWD/../../install "$sourceDir/libspatialindex"
 make -j 4 install
