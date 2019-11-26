3ae4d8c28645893fa865ee4462c37ae0f881ba55
Nathanael Hübbe <nathanael.huebbe@informatik.uni-hamburg.de>
2019-11-26 11:14:45 +0100

    turn preprocessor macro for benchmark parameterization into variable, and allow the data matrix to not be square

--- src/test/a-writeback-benchmark.c
+++ src/test/a-writeback-benchmark.c
@@ -26,32 +26,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define N (1 << 8)
-
-void initData(uint64_t (*data)[N]) {
-  for(int y = 0; y < N; y++) {
-    for(int x = 0; x < N; x++) {
-      data[y][x] = y*N + x;
+void initData(int height, int width, uint64_t (*data)[width]) {
+  for(int y = 0; y < height; y++) {
+    for(int x = 0; x < width; x++) {
+      data[y][x] = y*width + x;
     }
   }
 }
 
-bool dataIsCorrect(uint64_t (*data)[N]) {
-  for(int y = 0; y < N; y++) {
-    for(int x = 0; x < N; x++) {
-      if(data[y][x] != y*N + x) return false;
+bool dataIsCorrect(int height, int width, uint64_t (*data)[width]) {
+  for(int y = 0; y < height; y++) {
+    for(int x = 0; x < width; x++) {
+      if(data[y][x] != y*width + x) return false;
     }
   }
   return true;
 }
 
-//writes the data in the form of 1D slices of size [1][N]
-void writeData(esdm_dataset_t* dataset, esdm_dataspace_t* dataspace, uint64_t (*data)[N]) {
+//writes the data in the form of 1D slices of size [1][width]
+void writeData(esdm_dataset_t* dataset, esdm_dataspace_t* dataspace, int height, int width, uint64_t (*data)[width]) {
   esdm_statistics_t beforeStats = esdm_write_stats();
 
   int ret = ESDM_SUCCESS;
-  int64_t size[2] = {1, N};
-  for(int i = 0; i < N; i++) {
+  int64_t size[2] = {1, width};
+  for(int i = 0; i < height; i++) {
     int64_t offset[2] = {i, 0};
     esdm_dataspace_t *subspace;
     ret = esdm_dataspace_subspace(dataspace, 2, size, offset, &subspace);
@@ -69,24 +67,24 @@ void writeData(esdm_dataset_t* dataset, esdm_dataspace_t* dataspace, uint64_t (*
 
   //check statistics
   esdm_statistics_t afterStats = esdm_write_stats();
-  eassert(afterStats.bytesUser - beforeStats.bytesUser == N*sizeof(*data));
-  eassert(afterStats.bytesIo - beforeStats.bytesIo == N*sizeof(*data));
-  eassert(afterStats.requests - beforeStats.requests == N);
-  eassert(afterStats.fragments - beforeStats.fragments == N);
+  eassert(afterStats.bytesUser - beforeStats.bytesUser == height*sizeof(*data));
+  eassert(afterStats.bytesIo - beforeStats.bytesIo == height*sizeof(*data));
+  eassert(afterStats.requests - beforeStats.requests == height);
+  eassert(afterStats.fragments - beforeStats.fragments == height);
 }
 
 //fragmentSize is an array of two elements that gives the shape of the fragments to read
 //expectedReadFactor gives the expected factor by which the amount of data that's read from disk is larger than the amount of data requested
-void readData(esdm_dataset_t* dataset, esdm_dataspace_t* dataspace, uint64_t (*data)[N], int64_t* fragmentSize, int64_t expectedReadFactor, bool expectWriteBack) {
-  eassert(N%fragmentSize[0] == 0 && "if this fails, it's a bug in the test parameterization, not in ESDM itself");
-  eassert(N%fragmentSize[1] == 0 && "if this fails, it's a bug in the test parameterization, not in ESDM itself");
+void readData(esdm_dataset_t* dataset, esdm_dataspace_t* dataspace, int height, int width, uint64_t (*data)[width], int64_t* fragmentSize, int64_t expectedReadFactor, int64_t expectedRequestFactor, bool expectWriteBack) {
+  eassert(height%fragmentSize[0] == 0 && "if this fails, it's a bug in the test parameterization, not in ESDM itself");
+  eassert(width%fragmentSize[1] == 0 && "if this fails, it's a bug in the test parameterization, not in ESDM itself");
 
   esdm_statistics_t beforeStatsRead = esdm_read_stats();
   esdm_statistics_t beforeStatsWrite = esdm_write_stats();
 
   //perform the read
-  for(int y = 0; y < N; y += fragmentSize[0]) {
-    for(int x = 0; x < N; x += fragmentSize[1]) {
+  for(int y = 0; y < height; y += fragmentSize[0]) {
+    for(int x = 0; x < width; x += fragmentSize[1]) {
       esdm_dataspace_t* subspace;
       int64_t offset[2] = {y, x};
       int ret = esdm_dataspace_subspace(dataspace, 2, fragmentSize, offset, &subspace);
@@ -106,15 +104,17 @@ void readData(esdm_dataset_t* dataset, esdm_dataspace_t* dataspace, uint64_t (*d
   esdm_statistics_t afterStatsRead = esdm_read_stats();
   esdm_statistics_t afterStatsWrite = esdm_write_stats();
 
-  eassert(afterStatsRead.bytesUser - beforeStatsRead.bytesUser == N*sizeof(*data));
+  int64_t requestCount = height/fragmentSize[0] * width/fragmentSize[1];
+  int64_t fragmentCount = requestCount*expectedRequestFactor;
+  eassert(afterStatsRead.bytesUser - beforeStatsRead.bytesUser == height*sizeof(*data));
   eassert(afterStatsRead.bytesInternal - beforeStatsRead.bytesInternal == 0);
-  eassert(afterStatsRead.bytesIo - beforeStatsRead.bytesIo == expectedReadFactor*N*sizeof(*data));
-  eassert(afterStatsRead.requests - beforeStatsRead.requests == N);
+  eassert(afterStatsRead.bytesIo - beforeStatsRead.bytesIo == expectedReadFactor*height*sizeof(*data));
+  eassert(afterStatsRead.requests - beforeStatsRead.requests == requestCount);
   eassert(afterStatsRead.internalRequests - beforeStatsRead.internalRequests == 0);
-  eassert(afterStatsRead.fragments - beforeStatsRead.fragments == expectedReadFactor*N);
+  eassert(afterStatsRead.fragments - beforeStatsRead.fragments == fragmentCount);
 
-  eassert(afterStatsWrite.bytesInternal - beforeStatsWrite.bytesInternal == (expectWriteBack ? N*sizeof(*data) : 0));
-  eassert(afterStatsWrite.internalRequests - beforeStatsWrite.internalRequests == (expectWriteBack ? N : 0));
+  eassert(afterStatsWrite.bytesInternal - beforeStatsWrite.bytesInternal == (expectWriteBack ? height*sizeof(*data) : 0));
+  eassert(afterStatsWrite.internalRequests - beforeStatsWrite.internalRequests == (expectWriteBack ? requestCount : 0));
 }
 
 //TODO: Benchmark idea:
@@ -131,8 +131,10 @@ void readData(esdm_dataset_t* dataset, esdm_dataspace_t* dataspace, uint64_t (*d
 
 int main(int argc, char const *argv[]) {
   // prepare data
-  uint64_t (*data)[N] = malloc(N*sizeof(*data));
-  initData(data);
+  int height = 1<<9;
+  int width = 1<<7;
+  uint64_t (*data)[width] = malloc(height*sizeof(*data));
+  initData(height, width, data);
 
   // Interaction with ESDM
   esdm_status ret = esdm_init();
@@ -148,7 +150,7 @@ int main(int argc, char const *argv[]) {
   printf("\nTest 1: Measure worst case reading and the effect of writeback in this case\n");
 
   // define dataspace
-  int64_t bounds[2] = {N, N};
+  int64_t bounds[2] = {height, width};
   esdm_dataspace_t *dataspace;
 
   ret = esdm_dataspace_create(2, bounds, SMD_DTYPE_UINT64, &dataspace);
@@ -167,26 +169,26 @@ int main(int argc, char const *argv[]) {
 
   timer myTimer;
   start_timer(&myTimer);
-  writeData(dataset1, dataspace, data);
-  printf("write data: %.3fms\n", 1000*stop_timer(myTimer));
+  writeData(dataset1, dataspace, height, width, data);
+  printf("write data (%dx%d) as 1x%d fragments: %.3fms\n", height, width, width, 1000*stop_timer(myTimer));
 
-  memset(data, 0, N*sizeof(*data));
+  memset(data, 0, height*sizeof(*data));
   start_timer(&myTimer);
-  readData(dataset1, dataspace, data, (int64_t[2]){ 1, N}, 1, false);
+  readData(dataset1, dataspace, height, width, data, (int64_t[2]){ 1, width}, 1, 1, false);
   printf("read data as written: %.3fms\n", 1000*stop_timer(myTimer));
-  eassert(dataIsCorrect(data));
+  eassert(dataIsCorrect(height, width, data));
 
-  memset(data, 0, N*sizeof(*data));
+  memset(data, 0, height*sizeof(*data));
   start_timer(&myTimer);
-  readData(dataset1, dataspace, data, (int64_t[2]){ N, 1}, N, true);
-  printf("read data worst case: %.3fms\n", 1000*stop_timer(myTimer));
-  eassert(dataIsCorrect(data));
+  readData(dataset1, dataspace, height, width, data, (int64_t[2]){ height, 1}, width, height, true);
+  printf("read data as %dx1 fragments: %.3fms\n", height, 1000*stop_timer(myTimer));
+  eassert(dataIsCorrect(height, width, data));
 
-  memset(data, 0, N*sizeof(*data));
+  memset(data, 0, height*sizeof(*data));
   start_timer(&myTimer);
-  readData(dataset1, dataspace, data, (int64_t[2]){ N, 1}, 1, false);
-  printf("read data worst case repeat: %.3fms\n", 1000*stop_timer(myTimer));
-  eassert(dataIsCorrect(data));
+  readData(dataset1, dataspace, height, width, data, (int64_t[2]){ height, 1}, 1, 1, false);
+  printf("read data %dx1 fragments repeat: %.3fms\n", height, 1000*stop_timer(myTimer));
+  eassert(dataIsCorrect(height, width, data));
 
 
 
@@ -202,19 +204,19 @@ int main(int argc, char const *argv[]) {
   eassert(ret == ESDM_SUCCESS);
 
   start_timer(&myTimer);
-  writeData(dataset2, dataspace, data);
-  printf("write data: %.3fms\n", 1000*stop_timer(myTimer));
+  writeData(dataset2, dataspace, height, width, data);
+  printf("write data (%dx%d) as 1x%d fragments: %.3fms\n", height, width, width, 1000*stop_timer(myTimer));
 
   timer outerTimer;
   start_timer(&outerTimer);
-  for(int64_t width = N, hight = 1, readFactor = 1; width; width /= 2, hight *=2, readFactor *= 2) {
-    memset(data, 0, N*sizeof(*data));
+  for(int64_t fragmentSize[2] = {1, width}, readFactor = 1; fragmentSize[1] && fragmentSize[0] < height; fragmentSize[1] /= 2, fragmentSize[0] *=2, readFactor *= 2) {
+    memset(data, 0, height*sizeof(*data));
     bool expectWriteback = readFactor >= 8;
     start_timer(&myTimer);
-    readData(dataset2, dataspace, data, (int64_t[2]){ hight, width}, readFactor, expectWriteback);
-    printf("read data as %"PRId64"x%"PRId64" fragments: %.3fms%s\n", hight, width, 1000*stop_timer(myTimer), expectWriteback ? " (writeback)" : "");
+    readData(dataset2, dataspace, height, width, data, fragmentSize, readFactor, readFactor, expectWriteback);
+    printf("read data as %"PRId64"x%"PRId64" fragments: %.3fms%s\n", fragmentSize[0], fragmentSize[1], 1000*stop_timer(myTimer), expectWriteback ? " (writeback)" : "");
     if(expectWriteback) readFactor = 1;
-    eassert(dataIsCorrect(data));
+    eassert(dataIsCorrect(height, width, data));
   }
   printf("total: %.3fms\n", 1000*stop_timer(outerTimer));
 
