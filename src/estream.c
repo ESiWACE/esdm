8a3fa803cf429d3ff299c370e658df7059d32283
Julian M. Kunkel <juliankunkel@googlemail.com>
2020-05-02 17:43:13 +0100

    SCIL optional dependency seems to work.

---
+++ src/estream.c
@@ -0,0 +1,18 @@
+/* This file is part of ESDM.
+ * See the enclosed LICENSE */
+
+/**
+ * @file
+ * @brief Entry point for ESDM streaming implementation
+ */
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef HAVE_SCIL
+#include <scil.h>
+#endif
+
+#include <esdm-internal.h>
